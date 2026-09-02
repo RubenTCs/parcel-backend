af2f1890569e153f80a57b1ea8c873582e4028d9
Ruben <ruben.tobiachaiyadi@yahoo.com>
2026-09-02 20:26:23 +0700

    Spring boot init

---
+++ src/main/java/com/rubentc/jrtparcel/JrtParcelApplication.java
@@ -0,0 +1,13 @@
+package com.rubentc.jrtparcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrtParcelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JrtParcelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rubentc/jrtparcel/JrtParcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubentc.jrtparcel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrtParcelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
